a3ed051ef585c44a27af1be799d08c9cba5e918e
iamshaveenvf <shaveenfernando1@gmail.com>
2024-05-02 16:21:23 +0530

    error correction

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -10,7 +10,6 @@ import {
   ButtonContainer,
   MobileIcon,
   MobileMenu,
-  MobileNavLogo,
   MobileLink,
 } from "./NavbarStyledComponent";
 import { DiCssdeck } from "react-icons/di";
@@ -26,6 +25,7 @@ const Navbar = () => {
       <NavbarContainer>
         <NavLogo to="/">
           <a
+            href="/"
             style={{
               display: "flex",
               alignItems: "center",
--- src/themes/default.js
+++ src/themes/default.js
@@ -1,4 +1,4 @@
-export default {
+export const themes = {
   // Temp fonts
   fonts: {
     title: "Space Grotesk, sans-serif",
